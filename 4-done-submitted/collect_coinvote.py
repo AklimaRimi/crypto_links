22e3e10b15c602b2f2eb4cda3aa63a193a76bab3
Rimi <59701116+AklimaRimi@users.noreply.github.com>
2024-02-05 20:32:50 +0600

    4 final

--- 4-done-submitted/collect_coinvote.py
+++ 4-done-submitted/collect_coinvote.py
@@ -9,33 +9,34 @@ import multiprocessing as mp
 warnings.filterwarnings('ignore')
 
 
-driver = webdriver.Chrome()
 
-df = pd.read_csv(f'coinvote_links.csv')
 
-x =  len(df)//26
-print(x)
+# df = pd.read_csv(f'coinvote_links.csv')
 
-driver.maximize_window()
-for page in range(x,1030):
-    try:
-        lis = []
-        link = f'https://coinvote.cc/today&page={page}'
-        print(page)
-        driver.get(link)
-        time.sleep(2)
-        # window_height = driver.execute_script("return window.innerHeight;")
-        # driver.execute_script(f"window.scrollBy(0, {window_height*5});")
-        # time.sleep(1)
+# x =  len(df)//26
+# print(x)
 
-        li = driver.find_elements(By.XPATH,"//div[@class='coin-column redirect-coin']/a")
 
-        for i in li:
-            lis.append(i.get_attribute('href'))
-        df  = pd.DataFrame(lis)
-        df.to_csv('coinvote_links.csv',index=False,mode='a',header=False)
-    except:
-        continue
+# for page in range(x,1030):
+
+#     try:
+#         lis = []
+#         link = f'https://coinvote.cc/today&page={page}'
+#         print(page)
+#         driver.get(link)
+#         time.sleep(2)
+#         # window_height = driver.execute_script("return window.innerHeight;")
+#         # driver.execute_script(f"window.scrollBy(0, {window_height*5});")
+#         # time.sleep(1)
+
+#         li = driver.find_elements(By.XPATH,"//div[@class='coin-column redirect-coin']/a")
+
+#         for i in li:
+#             lis.append(i.get_attribute('href'))
+#         df  = pd.DataFrame(lis)
+#         df.to_csv('coinvote_links.csv',index=False,mode='a',header=False)
+#     except:
+#         continue
 
 
 df = pd.read_csv(f'deleted.csv')['Links'].values.tolist()
@@ -48,7 +49,8 @@ lis = [x for x in lis if (x not in df) and (x not in df3)]
 print(len(lis))
 
 def scrap(li):
-
+    driver = webdriver.Chrome()
+    driver.maximize_window()
     for i in li:
         try:
             print(i,'\n\n\n')
@@ -82,11 +84,11 @@ def scrap(li):
         except:
             print('Not Found')
  
-            
+    driver.close()
             
 if __name__ == '__main__':
 
-    cpu = 2
+    cpu = 3
 
     x = (len(lis)//cpu)+1
 
@@ -105,6 +107,5 @@ if __name__ == '__main__':
 
 
 
-    driver.close()
 
 
